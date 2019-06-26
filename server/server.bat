da31fd63d216679eb5840903112260b0cabe09b6
Tim S. Trense <trensetim@gmail.com>
2019-06-26 09:13:03 +0200

    bugfix clean checked-out version has not automatically created a ./server/logs directory

--- server/server.bat
+++ server/server.bat
@@ -1,5 +1,6 @@
 @echo off
 set JRE_HOME=%~dp0%..\jre
 set /P PORT=<conf\kira\port.txt
+if not exist logs mkdir logs
 start "Kira" /B "%JRE_HOME%\bin\java" -jar Server.jar >logs/start.log 2>&1
 exit 0
